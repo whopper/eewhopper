128c1dbf591a35a20b1f01628545c59ba0b2757a
Will Hopper <william.hopper@acquia.com>
2020-01-28 10:30:15 -0800

    Try switching to docroot

--- hooks/common/post-code-deploy/hello-world.sh
+++ hooks/common/post-code-deploy/hello-world.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
+# Is there a way to check for an existing install and not do this if there is
+# one?
+cd /var/www/html/eewhopper.ode14/docroot && drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
