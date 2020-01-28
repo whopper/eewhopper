79d0e3e1a63f5b7043fed1d67f7d19eeac20aae8
Will Hopper <william.hopper@acquia.com>
2020-01-28 11:37:54 -0800

    Add env debugging to hook

--- hooks/common/post-code-deploy/install-drupal.sh
+++ hooks/common/post-code-deploy/install-drupal.sh
@@ -2,6 +2,6 @@
 
 # Is there a way to check for an existing install and not do this if there is
 # one?
-cd /var/www/html/eewhopperode15/docroot
-pwd
+cd /var/www/html/eewhopperode16/docroot
+env
 yes | drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
