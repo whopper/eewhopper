104c7eb2d3d43febce2636af071ffcaba1fb67e0
Will Hopper <william.hopper@acquia.com>
2020-01-28 11:00:48 -0800

    Get more info from hook

--- hooks/common/post-code-deploy/hello-world.sh
+++ hooks/common/post-code-deploy/hello-world.sh
@@ -2,4 +2,6 @@
 
 # Is there a way to check for an existing install and not do this if there is
 # one?
-cd /var/www/html/eewhopper.ode14/docroot && drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
+cd /var/www/html/eewhopperode14/docroot
+pwd
+drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
