09e23cc8068f5b27741e3dfb219433d975ac07c4
Will Hopper <william.hopper@acquia.com>
2020-01-28 11:44:50 -0800

    Use script args to populate data

--- hooks/common/post-code-deploy/install-drupal.sh
+++ hooks/common/post-code-deploy/install-drupal.sh
@@ -2,6 +2,6 @@
 
 # Is there a way to check for an existing install and not do this if there is
 # one?
-cd /var/www/html/eewhopperode16/docroot
-env
-yes | drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
+cd /var/www/html/${2}/docroot
+pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+yes | drush site-install --account-name='admin' --account-mail="${1}@acquia.com" --site-mail="${1}@acquia.com" --account-pass="${pass}" --site-name='Drupal 8'
