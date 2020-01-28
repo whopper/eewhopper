05c09f43da985547d4f1acc2af6edd5e28be2bca
Will Hopper <william.hopper@acquia.com>
2020-01-28 11:28:07 -0800

    Add post-code-deploy hook to install drupal

--- hooks/common/post-code-deploy/hello-world.sh
+++ hooks/common/post-code-deploy/hello-world.sh
@@ -2,6 +2,6 @@
 
 # Is there a way to check for an existing install and not do this if there is
 # one?
-cd /var/www/html/eewhopperode14/docroot
+cd /var/www/html/eewhopperode15/docroot
 pwd
-drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
+yes | drush site-install --account-name='admin' --account-mail='foo@bar.com' --site-mail='foo@bar.com' --account-pass='randompass' --site-name='Drupal 8'
